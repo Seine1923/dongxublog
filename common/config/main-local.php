4dc163fec5528bdaf291be746475f5aec08f57cf
Seine1923 <xdyeung@163.com>
2016-05-24 13:01:50 +0800

    update gitignore

---
+++ common/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=dxdb',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@app/mailer',
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.163.com',
+                'username' => 'xdyeung@163.com',
+                'password' => 'YXD-1013',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+    ],
+];
